6801522d9dd2b80da344953ce5cd5d58dc90f53a
meloegel <meloegel@gmail.com>
2020-10-03 18:39:44 -0400

    fix

--- src/components/qualifications.js
+++ src/components/qualifications.js
@@ -21,7 +21,7 @@ const Qualifications = (key) => {
         <div className='hvr-grow' id='edu'>
             <div className="qualifications">
                 <h3>Education: Lambda School</h3>
-                <p>This needs more, maybe 'accomplishments', also thinking the experience descriptions i wrote in the resume. </p>
+                <p>This needs more, maybe 'accomplishments'? also thinking the experience descriptions i wrote in the resume? </p>
                 <button onClick={History}>Previous Work Experience <p id='buttonP'>(non-programming)</p></button>
                 <PrevWork />
                 <button onClick={History} id='closeButton' className='display'>Close</button>
