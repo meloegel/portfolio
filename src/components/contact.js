0f2ed8ac4724e1edc62e6427c1906d348ce9fa36
meloegel <meloegel@gmail.com>
2020-11-04 17:32:50 -0500

    updated linkedIn link

--- src/components/contact.js
+++ src/components/contact.js
@@ -6,7 +6,7 @@ const Contact = () => {
         <div className='hvr-grow' id='contactDiv' >
             <div className="contact">
                 <p>Email: Meloegel@gmail.com</p>
-                <p>LinkedIn: <a href='https://www.linkedin.com/in/mark-loegel-a43526b6/'>https://www.linkedin.com/in/mark-loegel-a43526b6/</a></p>
+                <p>LinkedIn: <a href='https://www.linkedin.com/in/mark-loegel/'>https://www.linkedin.com/in/mark-loegel/</a></p>
                 <p>Github: <a href='https://github.com/meloegel'>https://github.com/meloegel</a></p>
                 <p>Sourcerer.io: <a href='https://sourcerer.io/meloegel'>https://sourcerer.io/meloegel</a></p>
                 <p>Location: Madison Heights, Mi</p>
