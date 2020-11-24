5f99638fa277ca64f68e0892eb6df8cbb25fff10
meloegel <meloegel@gmail.com>
2020-11-24 13:22:34 -0500

    finished adding run tracker to projects section

--- src/components/projects.js
+++ src/components/projects.js
@@ -17,31 +17,30 @@ const Projects = () => {
                     <div className='projects'>
                         <p>How-To (Backend)</p>
                         <img src={require('../styles/Images/HowToImage.PNG')} alt='Project 2' className='projectPic' />
-                        <p>link: <a href='https://github.com/BW-how-2/Back-End' >github.com/BW-how-2/Back-End</a></p>
+                        <p>link: <a href='https://github.com/BW-how-2/Back-End'>github.com/BW-how-2/Back-End</a></p>
                     </div>
                     <div className='projects'>
                         <p>Cube Timer</p>
                         <img src={require('../styles/Images/CubeTimer.PNG')} alt='Project 3' className='projectPic' />
-                        <p>link: <a href='https://cube-timer.vercel.app/' >cube-timer.vercel.app/</a></p>
-                        <p>GitHub: <a href='https://github.com/meloegel/Cube-Timer' >github.com/meloegel/Cube-Timer</a></p>
+                        <p>link: <a href='https://cube-timer.vercel.app/'>cube-timer.vercel.app/</a></p>
+                        <p>GitHub: <a href='https://github.com/meloegel/Cube-Timer'>github.com/meloegel/Cube-Timer</a></p>
                     </div>
                     <div className='projects'>
                         <p>NASA Photo of the Day</p>
                         <img src={require('../styles/Images/NasaPOTD.PNG')} alt='Project 4' className='projectPic' />
-                        <p>link: <a href='https://nasa-photo-of-the-day-mloegel.vercel.app/' >nasa-photo-of-the-day-mloegel.vercel.app/</a></p>
-                        <p>GitHub: <a href='https://github.com/meloegel/nasa-photo-of-the-day' >github.com/meloegel/nasa-photo-of-the-day</a></p>
+                        <p>link: <a href='https://nasa-photo-of-the-day-mloegel.vercel.app/'>nasa-photo-of-the-day-mloegel.vercel.app/</a></p>
+                        <p>GitHub: <a href='https://github.com/meloegel/nasa-photo-of-the-day'>github.com/meloegel/nasa-photo-of-the-day</a></p>
                     </div>
                     <div className='projects'>
                         <p>Run Tracker FE</p>
-                        <img src='' alt='Project 5' className='projectPic' />
-                        <p>link:</p>
-                        <p>GitHub:<a href='https://github.com/meloegel/run-tracker-fe'>github.com/meloegel/run-tracker-fe</a></p>
+                        <img src={require('../styles/Images/runTrackerFE.PNG')} alt='Project 5' className='projectPic' />
+                        <p>link: <a href='https://run-tracker.vercel.app/'>run-tracker.vercel.app/</a></p>
+                        <p>GitHub: <a href='https://github.com/meloegel/run-tracker-fe'>github.com/meloegel/run-tracker-fe</a></p>
                     </div>
                     <div className='projects'>
                         <p>Run Tracker BE</p>
-                        <img src='' alt='Project 6' className='projectPic' />
-                        <p>link:</p>
-                        <p>GitHub:<a href='https://github.com/meloegel/run-tracker-be'>github.com/meloegel/run-tracker-be</a></p>
+                        <img src={require('../styles/Images/runTrackerBE.PNG')} alt='Project 6' className='projectPic' />
+                        <p>Link: <a href='https://github.com/meloegel/run-tracker-be'>github.com/meloegel/run-tracker-be</a></p>
                     </div>
                 </div>
             </div>
