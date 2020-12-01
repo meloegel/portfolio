efb97f2b340c14c0279d5b1643b9803e8e910bc1
meloegel <meloegel@gmail.com>
2020-11-30 19:34:43 -0500

    added education

--- src/components/qualifications.js
+++ src/components/qualifications.js
@@ -20,8 +20,21 @@ const Qualifications = (key) => {
     return (
         <div className='hvr-grow' id='edu'>
             <div className="qualifications">
-                <h3>Education: Lambda School</h3>
-                <h3>Previous Education: Oakland University (Psychology)</h3>
+                <div className='edu'>
+                    <div>
+                        <h3>Education: Lambda School</h3>
+                        <p>March 2020 - Nov 2020</p>
+                        <p>Participated in the Lambda Leadership program</p>
+                        <p>Part of the Lambda X Endorsement program</p>
+                    </div>
+                    <div>
+                        <h3>Previous Education: Oakland University</h3>
+                        <p>Bachlors Degree in Psychology</p>
+                        <p>2008-2012</p>
+                        <p>Division 1 Athelete in Swimming</p>
+                        <p>4 year Conference Champions</p>
+                    </div>
+                </div>
                 <button onClick={History}>Previous Work Experience <p id='buttonP'>(non-programming)</p></button>
                 <PrevWork />
                 <button onClick={History} id='closeButton' className='display'>Close</button>
