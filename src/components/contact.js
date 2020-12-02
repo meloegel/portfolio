d4c09ec3640943367c50ac8eade6744ed0b5813b
meloegel <meloegel@gmail.com>
2020-12-02 17:31:43 -0500

    added icons to contact

--- src/components/contact.js
+++ src/components/contact.js
@@ -5,11 +5,11 @@ const Contact = () => {
     return (
         <div className='hvr-grow' id='contactDiv' >
             <div className="contact">
-                <p>Email: Meloegel@gmail.com</p>
-                <p>LinkedIn: <a href='https://www.linkedin.com/in/mark-loegel/'>https://www.linkedin.com/in/mark-loegel/</a></p>
-                <p>Github: <a href='https://github.com/meloegel'>https://github.com/meloegel</a></p>
-                <p>Sourcerer.io: <a href='https://sourcerer.io/meloegel'>https://sourcerer.io/meloegel</a></p>
-                <p>Location: Madison Heights, MI</p>
+                <p>Email <i class="fas fa-envelope-square"></i>: Meloegel@gmail.com</p>
+                <p>LinkedIn <i class="fab fa-linkedin"></i>: <a href='https://www.linkedin.com/in/mark-loegel/'>https://www.linkedin.com/in/mark-loegel/</a></p>
+                <p>Github <i class="fab fa-github"></i>: <a href='https://github.com/meloegel'>https://github.com/meloegel</a></p>
+                <p>Sourcerer.io <i class="fab fa-stripe-s"></i>: <a href='https://sourcerer.io/meloegel'>https://sourcerer.io/meloegel</a></p>
+                <p>Location <i class="fas fa-location-arrow"></i>: Madison Heights, MI</p>
             </div>
         </div>
     )
