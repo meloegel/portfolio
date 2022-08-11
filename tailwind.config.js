5cb8870371be12cba93de22ec20b86d15998c322
meloegel <meloegel@gmail.com>
2022-08-10 22:53:43 -0400

    start initial work on update for ford

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    maxWidth: {
+      'foo': "400px"
+    },
+    extend: {
+      backgroundImage: {
+        'headerPhoto': url("../Images/HeaderPhoto.jpg"),
+      },
+      height: {
+        'h400px': '25rem',
+      }
+    },
+  },
+  variants: {
+    extend: {
+      opacity: ['disabled'],
+    },
+  },
+  plugins: [],
+}
