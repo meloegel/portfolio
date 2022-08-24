09ba3661c9da83e759f79d5a2b1becfd8a8cd497
meloegel <meloegel@gmail.com>
2022-08-23 21:29:46 -0400

    updated skills section

--- src/components/skills.js
+++ src/components/skills.js
@@ -6,15 +6,15 @@ const Skills = () => {
     return (
         <div className="w-full text-center mb-1.5">
             <div className='hvr-grow relative bg-gray-500 p-1.5 rounded-3xl text-left mx-auto my-8 w-11/12'>
-                <h3 ><i class="fas fa-medal"/> Skills <i class="fas fa-medal"/></h3>
+                <h3 ><i class="fas fa-medal" /> Skills <i class="fas fa-medal" /></h3>
                 <div className="flex mb-8 flex-col md:flex-row">
                     <div className="m-auto text-white text-center p-1 md:p-0 ">
                         <ul>
-                            <h4>Core Skills</h4>
-                            <li><i className="fab fa-html5"/> HTML</li>
-                            <li><i className="fab fa-css3-alt"/> CSS</li>
-                            <li><i className="fab fa-js-square"/> Javascript</li>
-                            <li><i className="fab fa-python"/> Python</li>
+                            <h4 className='mb-1'>CORE SKILLS</h4>
+                            <li><i className="fab fa-html5" /> HTML</li>
+                            <li><i className="fab fa-css3-alt" /> CSS</li>
+                            <li><i className="fab fa-js-square" /> Javascript</li>
+                            <li><i className="fab fa-python" /> Python</li>
                             <li>Typescript</li>
                             <li>Java</li>
                             <li>Kotlin</li>
@@ -23,13 +23,13 @@ const Skills = () => {
                     </div>
                     <div className="m-auto text-white text-center p-1 md:p-0 ">
                         <ul>
-                            <h4>Front-End</h4>
-                            <li><i className="fab fa-react"/> ReactJS</li>
-                            <li><i class="fas fa-map-marked-alt"/> Mapbox</li>
-                            <li><i className="fab fa-bootstrap"/> Bootstrap & ReactStrap</li>
-                            <li><i class="fas fa-chart-bar"/> Plotly-Js</li>
-                            <li><i className="fab fa-less"/> LESS</li>
-                            <li><i class="fab fa-sass"/> SASS</li>
+                            <h4 className='mb-1'>FRONT-END</h4>
+                            <li><i className="fab fa-react" /> ReactJS</li>
+                            <li><i class="fas fa-map-marked-alt" /> Mapbox</li>
+                            <li><i className="fab fa-bootstrap" /> Bootstrap & ReactStrap</li>
+                            <li><i class="fas fa-chart-bar" /> Plotly-Js</li>
+                            <li><i className="fab fa-less" /> LESS</li>
+                            <li><i class="fab fa-sass" /> SASS</li>
                             <li>Redux</li>
                             <li>Tailwind Css</li>
                             <li>Semantic UI</li>
@@ -39,8 +39,8 @@ const Skills = () => {
                     </div>
                     <div className="m-auto text-white text-center p-1 md:p-0 ">
                         <ul>
-                            <h4>Back-End</h4>
-                            <li><i className="fab fa-node-js"/> NodeJS</li>
+                            <h4 className='mb-1'>BACK-END</h4>
+                            <li><i className="fab fa-node-js" /> NodeJS</li>
                             <li>Spring</li>
                             <li>PostgreSQL</li>
                             <li>SQLite3</li>
@@ -50,7 +50,7 @@ const Skills = () => {
                     </div>
                     <div className="m-auto text-white text-center p-1 md:p-0 ">
                         <ul>
-                            <h4>Testing</h4>
+                            <h4 className='mb-1'>TESTING</h4>
                             <li>Cypress</li>
                             <li>Jest</li>
                             <li>Supertest</li>
