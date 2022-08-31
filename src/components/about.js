a21209d2b91952064c3b6e0d991279dc99fed148
meloegel <meloegel@gmail.com>
2022-08-30 20:54:13 -0400

    updated about section

--- src/components/about.js
+++ src/components/about.js
@@ -7,7 +7,7 @@ const About = () => {
             <p className='border-dashed border-2 border-white my-2'></p>
             <div className='hvr-grow mt-1 bg-gray-500 rounded-3xl w-11/12 m-auto' >
                 <div className="w-11/12 m-auto p-3">
-                    <h4>I am a full stack web developer that specializes in React and Node. I have a passion for programming and love to learn as much as I can about it. My experience at Lambda school has not only given me leadership and problem solving skills but also the confidence and ability to be  able to teach myself. It is something I will use the rest of my life in the pursuit to gain knowledge in the ever changing industry. I am currently based near Detroit Michigan (Madison Heights) and I am excited to potentially work with you! </h4>
+                    <h4>I am a full stack web developer that has a passion for programming and love learning as much as I can about it. My experience at Lambda school has not only given me leadership and problem solving skills but also the confidence and ability to be able to teach myself. It is something I will use the rest of my life in the pursuit to gain knowledge in the ever changing industry. I am currently based near Detroit Michigan (Madison Heights) and I am excited to potentially work with you!</h4>
                 </div>
             </div>
         </div>
